bf0acd877735a1c692c249096ca58cb251cb65d8
Botesla <12291772+mrbotart@users.noreply.github.com>
2022-08-26 16:19:58 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.pause(100)
     strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     basic.pause(100)
+    if (input.buttonIsPressed(Button.A)) {
+        RingbitCar.forward()
+    } else if (input.buttonIsPressed(Button.B)) {
+        RingbitCar.back()
+    } else {
+        RingbitCar.brake()
+    }
 })
